38eddd53c7ca18044a6d7a678e120eb13da79526
Manojdhage-git <manojdhage1341@gmail.com>
2024-06-16 08:18:23 +0000

    Conversion

---
+++ 01_basics/ConversionOfDatatypes.js
@@ -0,0 +1,15 @@
+// conversion of number into string
+let Id = 123
+let IdString = String(Id)
+console.log(IdString)
+console.log(typeof Id)
+console.log(typeof IdString)
+
+
+// conversion of num+string
+
+let name = "1manoj"
+let Coname = Number(name)
+console.log(Coname)
+console.log(typeof Coname)
+

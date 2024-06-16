aa9836f2b5ecb60838f37229540cd908620af15b
Manojdhage-git <manojdhage1341@gmail.com>
2024-06-16 06:54:32 +0000

    Datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,21 @@
+// Datatypes ------
+// primitive data types---
+// string 
+// numbers 
+// boolean types true or false 
+// null
+//undefined
+//symbol -- used for uniqueness...
+
+let name = "manoj"
+let age = 13
+let isLoggedIn = false
+let string = "manoj"
+let state; // undefind
+let m = null // null means empty
+
+console.log(typeof age)
+console.log(typeof m)
+
+//non primitive
+// object 

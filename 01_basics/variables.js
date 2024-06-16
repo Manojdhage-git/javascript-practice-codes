c08a91640b13977be99d1b4b6da795b648acb8f0
Manojdhage-git <manojdhage1341@gmail.com>
2024-06-16 06:35:19 +0000

    container config done

---
+++ 01_basics/variables.js
@@ -0,0 +1,17 @@
+// variables ----
+// constant - const 
+// let 
+// var 
+
+
+const accountId = 15788758  // can't change value
+let accountEmail = "manojdhage@gmail.com" // can change
+var accoutPassward = "09180919840" // dont use var because of scope problem
+accoutCity = "nagar"
+
+// accountId = 4 
+console.log(accountId)
+
+accountEmail = "md@mg.com"
+accoutPassward = "031380"
+console.table([ accountId, accountEmail, accoutPassward, accoutCity])

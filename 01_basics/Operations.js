446ffa7d6f369c6281b4ba471548231c7e56fab3
Manojdhage-git <manojdhage1341@gmail.com>
2024-06-16 08:47:04 +0000

    operations

---
+++ 01_basics/Operations.js
@@ -0,0 +1,39 @@
+// *************** Operations ***************
+
+let no = 3
+let negNo = -no
+// console.log(negNo) 
+
+// console.log( 1+2)
+// console.log(1-2)
+// console.log(1*2)
+// console.log(1/2)
+// console.log(1**2)
+// console.log(1%2)
+
+
+let str1 = "MANOJ"
+let str2 = " Dhage"
+let str3 = str1 + str2
+console.log(str3)
+
+
+// .................
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + 2 + 2)
+// console.log(1 + 2 + "2")
+
+
+// prefix and postfix increment operator...........
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);

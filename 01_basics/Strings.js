52b052f0362fe79c24507a915044cde3b31fbc52
Manojdhage-git <manojdhage1341@gmail.com>
2024-06-18 04:27:08 +0000

    Strings

---
+++ 01_basics/Strings.js
@@ -0,0 +1,16 @@
+
+let Applecol = "Red"
+let Bananacol = "Yellow"
+let Orangecol = "Orange"
+
+console.log(`Colors of fruits is ${Applecol} ${Bananacol}  ${Orangecol}`)
+
+// ............... 
+
+const Color = new String('Red')
+
+console.log(Color[1]);
+console.log(Color.__proto__);
+
+// toUppercase() , toLowercase(), charAt(2) , length, indexOf('t') , Substring , slice ,trim
+// replace , includes , spilt-- 

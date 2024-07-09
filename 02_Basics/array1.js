ef870aa76c4d07d0c7d00a4a0a80375a9628d27c
Manojdhage-git <manojdhage1341@gmail.com>
2024-07-09 14:12:45 +0000

    Array

---
+++ 02_Basics/array1.js
@@ -0,0 +1,14 @@
+let Arry = [11,22,44] // array is object
+let ar = new Array(1,2,3)
+
+
+// resizable 
+// copy operation create shallow copy..
+// shallow copy is also change in main array
+
+
+// methods 
+
+// push , pop ,unshift , shift , includes - present or not , indexof , join() - convert into string..
+// slice --- it can not manipulate array.
+// splice -- original array also removes it manipulates array

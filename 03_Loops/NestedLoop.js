b06710ca5993e0eecdd976312663e8f6052d1326
Manojdhage-git <manojdhage1341@gmail.com>
2025-03-22 03:09:15 +0000

    Loops

---
+++ 03_Loops/NestedLoop.js
@@ -0,0 +1,6 @@
+//  For Loop inside loop is Nested Loop
+
+for(let i=1; i<=5;i++){
+    for(let j=1;j<=5; j++)
+        console.log(i+j);
+}
---
+++ 03_Loops/WhileLoop.js
@@ -0,0 +1,9 @@
+// same work as for loop
+
+let i=1;
+
+while(i<=5){
+    console.log("gm")
+
+    i++;
+}

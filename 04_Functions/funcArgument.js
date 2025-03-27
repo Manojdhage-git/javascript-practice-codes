b631c624ceaa0ac0edb6827befd2525c86736119
Manojdhage-git <manojdhage1341@gmail.com>
2025-03-27 14:06:51 +0000

    function with arguments

---
+++ 04_Functions/funcArgument.js
@@ -0,0 +1,18 @@
+//  Function with arguments
+
+// function to print name
+function printname(name){
+    console.log(name);
+}
+
+printname("manoj Dhage");
+
+
+// average of three numbers
+
+function avg(num1,num2,num3){
+    
+    console.log((num1+num2+num3)/3);
+}
+
+avg(44,55,44)

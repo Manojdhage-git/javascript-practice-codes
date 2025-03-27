54bc56476dac7765529a93d79a43bca14ab86615
Manojdhage-git <manojdhage1341@gmail.com>
2025-03-27 14:15:55 +0000

    return keyword

---
+++ 04_Functions/Returnkeyword.js
@@ -0,0 +1,14 @@
+
+// return values
+
+function sum(a,b){
+    return a+b;
+
+    // code will not work after return statement...........
+}
+
+console.log(sum(7,3));
+
+let doublee=sum(sum(1,2),3);
+
+console.log(doublee);

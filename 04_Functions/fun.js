86f4aaf293803833f70d34310d71d2ef7bb88375
Manojdhage-git <manojdhage1341@gmail.com>
2025-03-27 13:45:36 +0000

    basic Function

---
+++ 04_Functions/fun.js
@@ -0,0 +1,26 @@
+//  Basic Syantax of function
+
+function Hello(){
+    console.log("Hello")
+}
+
+Hello();
+
+
+// Function print name
+
+function printName(){
+    console.log("Manoj Dhage")
+}
+
+printName();
+
+// print 1 to 5 using function
+
+function oneToFIve(){
+    for( let i =1 ; i<=5;i++){
+        console.log(i)
+    }
+}
+
+oneToFIve();

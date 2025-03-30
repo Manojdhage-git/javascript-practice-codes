bd9692783143b1579033727cdaefb8f6b5ddcdf2
Manojdhage-git <manojdhage1341@gmail.com>
2025-03-30 16:14:36 +0000

    arrowFunction

---
+++ 05_MoreInFunction/ArowFunction.js
@@ -0,0 +1,14 @@
+// Another way to write function
+// but write in thee variable
+
+const sum=(a,b)=>{
+    return a+b;
+}
+
+console.log(sum(10,20));
+
+// implicit return
+
+const mul = (a,b)=>(a*b);
+
+console.log(mul(20,44));
---
+++ 05_MoreInFunction/TryCatch.js
@@ -0,0 +1,7 @@
+// handling  the errors........
+
+try{console.log(a)
+
+}catch(err){
+    console.log("a is not defined and error is handled..........")
+}
